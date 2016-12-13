217de95be3e0dbeaedc1a437bb9963d5d335d424
Kiran Thapa <kthapa.swiss@gmail.com>
2016-12-13 02:08:43 -0700

    Add age group info in popup

--- script/js/location-summary.js
+++ script/js/location-summary.js
@@ -3,13 +3,13 @@
  */
 
 d3.csv("../data/BX-Users.csv", function(data) {
-    var totalCount = 0;
+
+
     var locationsDictionary = {};
-    var filteredLocationsDictionary = {};
     var locationssArray = new Array();
 
+    // Find countries and number of readers/raters in each of them
     for (var i = 0; i < data.length; i++) {
-        totalCount += 1;
         var locationArray = data[i]["Location"].toString().split(", ");
         var country = locationArray[2];
 
@@ -20,6 +20,62 @@ d3.csv("../data/BX-Users.csv", function(data) {
         }
     }
 
+    // Find age group within a country
+    var ageGroupDictionary = {};
+    for (var i = 0; i < data.length; i++) {
+        var locationArray = data[i]["Location"].toString().split(", ");
+        var country = locationArray[2];
+
+        if (locationsDictionary[country] > 50) {
+            if (!(country in ageGroupDictionary)) {
+                ageGroupDictionary[country] = {"0to9": 0, "10to19": 0, "20to29": 0, "30to39": 0, "40to49": 0,
+                                                "50to59": 0, "60to69": 0, "70to79": 0, "80to89": 0, "90to99": 0, "100+": 0};
+            }
+            var age = data[i]["Age"];
+            switch (true) {
+                case (age > 0 && age <= 9):
+                    ageGroupDictionary[country]["0to9"]++;
+                    break;
+                case (age >= 10 && age <= 19):
+                    ageGroupDictionary[country]["10to19"]++;
+                    break;
+                case (age >= 20 && age <= 29):
+                    ageGroupDictionary[country]["20to29"]++;
+                    break;
+                case (age >= 30 && age <= 39):
+                    ageGroupDictionary[country]["30to39"]++;
+                    break;
+                case (age >= 40 && age <= 49):
+                    ageGroupDictionary[country]["40to49"]++;
+                    break;
+                case (age >= 50 && age <= 59):
+                    ageGroupDictionary[country]["50to59"]++;
+                    break;
+                case (age >= 60 && age <= 69):
+                    ageGroupDictionary[country]["60to69"]++;
+                    break;
+                case (age >= 70 && age <= 79):
+                    ageGroupDictionary[country]["70to79"]++;
+                    break;
+                case (age >= 80 && age <= 89):
+                    ageGroupDictionary[country]["80to89"]++;
+                    break;
+                case (age >= 90 && age <= 99):
+                    ageGroupDictionary[country]["90to99"]++;
+                    break;
+                case (age >= 100):
+                    ageGroupDictionary[country]["100+"]++;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+
+    console.log(ageGroupDictionary);
+
+
     for (var key in locationsDictionary) {
         if (locationsDictionary[key] > 50) {
             locationssArray.push({label: key.toUpperCase(), value: locationsDictionary[key]});
@@ -29,8 +85,6 @@ d3.csv("../data/BX-Users.csv", function(data) {
     locationssArray.sort(function(a,b) {
         return b.value - a.value;
     });
-
-    console.log(locationssArray);
 
     data = locationssArray;
 
@@ -108,7 +162,20 @@ d3.csv("../data/BX-Users.csv", function(data) {
             div.style("left", d3.event.pageX+10+"px");
             div.style("top", d3.event.pageY-25+"px");
             div.style("display", "inline-block");
-            div.html((d.label)+"<br>"+"Total: "+(d.value)+"<br>"+"0-10"+": "+"20"+"<br>"+"0-10"+": "+"20");
+            div.html((d.label)+"<br>"
+                +"Total: "+(d.value)+"<br><br>"
+                +"0-9"+": "+(ageGroupDictionary[d.label.toLowerCase()]['0to9'])+"<br>"
+                +"10-19"+": "+(ageGroupDictionary[d.label.toLowerCase()]['10to19'])+"<br>"
+                +"20-29"+": "+(ageGroupDictionary[d.label.toLowerCase()]['20to29'])+"<br>"
+                +"30-39"+": "+(ageGroupDictionary[d.label.toLowerCase()]['30to39'])+"<br>"
+                +"40-49"+": "+(ageGroupDictionary[d.label.toLowerCase()]['40to49'])+"<br>"
+                +"50-59"+": "+(ageGroupDictionary[d.label.toLowerCase()]['50to59'])+"<br>"
+                +"60-69"+": "+(ageGroupDictionary[d.label.toLowerCase()]['60to69'])+"<br>"
+                +"70-79"+": "+(ageGroupDictionary[d.label.toLowerCase()]['70to79'])+"<br>"
+                +"80-89"+": "+(ageGroupDictionary[d.label.toLowerCase()]['80to89'])+"<br>"
+                +"90-99"+": "+(ageGroupDictionary[d.label.toLowerCase()]['90to99'])+"<br>"
+                +"100+"+": "+(ageGroupDictionary[d.label.toLowerCase()]['100+'])+"<br>");
+
         });
     bar
         .on("mouseout", function(d){
